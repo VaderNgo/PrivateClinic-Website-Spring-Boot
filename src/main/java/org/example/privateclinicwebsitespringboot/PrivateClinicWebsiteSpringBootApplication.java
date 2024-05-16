58d5c9f5ce4a914c4995c1d5d7717f637d4cccc7
VaderNgo <vaderngo@gmail.com>
2024-05-16 09:04:30 +0700

    Intial Commit

---
+++ src/main/java/org/example/privateclinicwebsitespringboot/PrivateClinicWebsiteSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.example.privateclinicwebsitespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateClinicWebsiteSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrivateClinicWebsiteSpringBootApplication.class, args);
+    }
+
+}
